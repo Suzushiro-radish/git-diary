2af7338d7681b70ba39655e31767950a49e5d03a
DaikiTakagi <d.takagi.radished@gmail.com>
2025-02-08 23:56:46 +0900

    Add chrono dependency and implement reflog retrieval for the last 7 days

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 use anyhow::Result;
+use chrono::{Duration, Local};
 
 fn main() -> Result<()> {
     let path = std::env::current_dir()?;
     let repo = git2::Repository::open(path)?;
-    let head = repo.head()?;
-    let head = head.peel_to_commit()?;
-    let head_id = head.message().unwrap_or("default message");
-    println!("HEAD: {}", head_id);
+    let reflogs = repo.reflog("HEAD")?;
+    let reflogs = reflogs.iter();
+
+    let now = Local::now();
+    let start = (now - Duration::days(7)).timestamp();
+
+    let mut messages: Vec<String> = Vec::new();
+
+    for reflog in reflogs {
+        let time = reflog.committer().when();
+        if time.seconds() < start {
+            break;
+        }
+        messages.push(reflog.message().unwrap_or("No message").to_string());
+    }
+
+    println!("Last 7 days commits:");
+    for message in messages.iter().rev() {
+        println!("{}", message);
+    }
 
     Ok(())
 }
