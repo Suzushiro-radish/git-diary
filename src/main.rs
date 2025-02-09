5f9642c723f8c06c0ed9b2d9a6d9465ff9286157
DaikiTakagi <d.takagi.radished@gmail.com>
2025-02-09 21:45:15 +0900

    Refactor reflog handling to use a Commit struct for better organization and display of commit messages with timestamps

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use std::fmt::Display;
+
 use anyhow::Result;
-use chrono::{Duration, Local};
+use chrono::{DateTime, Duration, Local};
+
+struct Commit {
+    message: String,
+    time: i64,
+}
+
+impl Commit {
+    fn new(message: String, time: i64) -> Self {
+        Self { message, time }
+    }
+
+    pub fn datetime(&self) -> Option<String> {
+        let datetime = DateTime::from_timestamp(self.time, 0);
+        datetime.map(|dt| dt.to_string())
+    }
+}
+
+impl Display for Commit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.datetime().unwrap_or("Invalid Date".to_string()), self.message)
+    }
+}
 
 fn main() -> Result<()> {
     let path = std::env::current_dir()?;
@@ -10,18 +34,21 @@ fn main() -> Result<()> {
     let now = Local::now();
     let start = (now - Duration::days(7)).timestamp();
 
-    let mut messages: Vec<String> = Vec::new();
+    let mut commits: Vec<Commit> = Vec::new();
 
     for reflog in reflogs {
         let time = reflog.committer().when();
         if time.seconds() < start {
             break;
         }
-        messages.push(reflog.message().unwrap_or("No message").to_string());
+        commits.push(Commit::new(
+            reflog.message().unwrap_or("No message").to_string(),
+            reflog.committer().when().seconds(),
+        ));
     }
 
     println!("Last 7 days commits:");
-    for message in messages.iter().rev() {
+    for message in commits.iter().rev() {
         println!("{}", message);
     }
 
