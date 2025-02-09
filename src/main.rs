335bfd92dab26437b2a597a402db29219e78de98
DaikiTakagi <d.takagi.radished@gmail.com>
2025-02-09 22:36:21 +0900

    Implement simple commit summary generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::fmt::Display;
-
 use anyhow::Result;
+use async_openai::{
+    types::{ChatCompletionRequestSystemMessageArgs, CreateChatCompletionRequestArgs},
+    Client,
+};
 use chrono::{DateTime, Duration, Local};
+use std::fmt::Display;
 
 struct Commit {
     message: String,
@@ -21,11 +24,17 @@ impl Commit {
 
 impl Display for Commit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {}", self.datetime().unwrap_or("Invalid Date".to_string()), self.message)
+        write!(
+            f,
+            "{}: {}",
+            self.datetime().unwrap_or("Invalid Date".to_string()),
+            self.message
+        )
     }
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let path = std::env::current_dir()?;
     let repo = git2::Repository::open(path)?;
     let reflogs = repo.reflog("HEAD")?;
@@ -50,6 +59,35 @@ fn main() -> Result<()> {
     println!("Last 7 days commits:");
     for message in commits.iter().rev() {
         println!("{}", message);
+    }
+
+    let openai_client = Client::new();
+
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-4o")
+        .max_tokens(1000_u32)
+        .messages([
+            ChatCompletionRequestSystemMessageArgs::default()
+                .content("You are a software developer who has been working on a project for the last 7 days. You have been making commits to a git repository. You want to summarize the commits you have made in the last 7 days.")
+                .build()?
+                .into(),
+            ChatCompletionRequestSystemMessageArgs::default()
+                .content(commits.iter().map(|commit| commit.to_string()).collect::<Vec<String>>().join("\n"))
+                .build()?
+                .into(),
+        ])
+        .build()?;
+
+    let response = openai_client.chat().create(request).await?;
+
+    println!("Summary:");
+
+    for choice in response.choices {
+        println!("------------------------------------");
+        println!(
+            "{}",
+            choice.message.content.unwrap_or("No content".to_string())
+        );
     }
 
     Ok(())
